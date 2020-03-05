4ce9b7b9f113cfb09ec567d637b08f3fc9675c82
Димачка Шорин <patrick_lz@live.com>
2020-03-05 22:46:34 +0400

    d

--- Restaurant menu/Controllers/DishController.cs
+++ Restaurant menu/Controllers/DishController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Restaurant_menu.Data;
 using Restaurant_menu.Models;
+using System;
+using System.Linq;
 
 namespace Restaurant_menu.Controllers
 {
@@ -13,10 +15,33 @@ namespace Restaurant_menu.Controllers
             _dbContext = dbContext;
         }
 
-        [HttpPost]
-        public IActionResult Add([FromBody] Dish dish)
+        [HttpPut]
+        public IActionResult Index([FromBody] Dish dish)
         {
-            return Ok();
+            dish.Adding = DateTime.Now;
+
+            if (_dbContext.Dishes.FirstOrDefault(p => p.Name == dish.Name) == null)
+            {
+                _dbContext.Dishes.Add(dish);
+                return Ok();
+            }
+
+            return null;
+        }
+
+        [HttpDelete]
+        public IActionResult Index([FromBody] Dish dish)
+        {
+            Dish deleteDish = _dbContext.Dishes.FirstOrDefault(p => p.Name == dish.Name &&
+            p.Description == dish.Description && p.Cost == dish.Cost && p.Weight == dish.Weight &&
+            p.Calories == dish.Calories);
+
+            if (deleteDish != null) { 
+                _dbContext.Dishes.Remove(deleteDish);
+                return Ok();
+            }
+
+            return null;
         }
     }
 }
