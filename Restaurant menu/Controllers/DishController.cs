eddf533ed513ca9a10d505df93e1cb98d924a66b
Anton Kravchenko <anton.kravchenko@simbirsoft.com>
2020-03-09 19:33:47 +0400

    Проверка

--- Restaurant menu/Controllers/DishController.cs
+++ Restaurant menu/Controllers/DishController.cs
@@ -25,7 +25,6 @@ namespace Restaurant_menu.ControllerBase
             return Json(dishes);
         }
 
-        //TODO Post используется для обновления данных
         [HttpPost]
         public IActionResult Create([FromBody] Dish dish)
         {
@@ -35,7 +34,6 @@ namespace Restaurant_menu.ControllerBase
             return Ok();
         }
 
-        //TODO Put используется для создания данных
         [HttpPut]
         public IActionResult Update([FromBody] Dish dish)
         {
